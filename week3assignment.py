fbebf304eea6a4a6bf12738572cc0945616fd3f4
kamau552 <faithkamau730@gmail.com>
2025-08-18 15:03:01 +0300

    Functions and if statements week3 python assignment

---
+++ week3assignment.py
@@ -0,0 +1,27 @@
+#Create a function named calculate_discount(price, discount_percent) 
+# that calculates the final price after applying a discount. 
+# The function should take the original price (price) and the discount percentage 
+# (discount_percent) as parameters. If the discount is 20% or higher, 
+# apply the discount; otherwise, return the original price.
+#Using the calculate_discount function, prompt the user to enter the 
+# original price of an item and the discount percentage. Print the final
+# price after applying the discount, or if no discount was applied, print the original price.
+
+
+
+def calculate_discount(price, discount_percent): 
+    if discount_percent >= 20:
+        return price * (1 - discount_percent / 100)
+    else:
+        return price
+    
+#user prompt (I encountered a Typeerror here '>=' not supported between instances of 'str' and 'int'
+# this is because i wasent passing an integer or float)
+price = float(input("Enter original price of the item:"))
+discount_percentage = float(input("Enter percentage discount of item:"))
+#final calculation 
+final_price = calculate_discount(price, discount_percentage)
+
+print("Final price:", final_price)
+
+
